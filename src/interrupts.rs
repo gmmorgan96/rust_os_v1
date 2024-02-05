727548ee595655d2ce4d64e4314985ee1e6635b9
gmmorgan1996 <griffin.mrogan@gmail.com>
2024-02-05 14:20:31 -0500

    chore: test

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,12 +1,6 @@
-use x86_64::structures::idt::InterruptDescriptorTable;
-use lazy_static::lazy_static;
 use crate::println;
-
-extern "x86-interrupt" fn breakpoint_handler(
-    stack_frame: InterruptStackFrame)
-{
-    println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
-}
+use lazy_static::lazy_static;
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
@@ -18,4 +12,8 @@ lazy_static! {
 
 pub fn init_idt() {
     IDT.load();
+}
+
+extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
+    println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
