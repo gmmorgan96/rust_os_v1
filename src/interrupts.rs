bbeb005aaf34b59f77e00ee2bf2a8c155e2b41ce
gmmorgan1996 <griffin.mrogan@gmail.com>
2024-02-05 14:17:53 -0500

    chore: update

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -2,11 +2,6 @@ use x86_64::structures::idt::InterruptDescriptorTable;
 use lazy_static::lazy_static;
 use crate::println;
 
-pub fn init_idt() {
-    let mut idt = InterruptDescriptorTable::new();
-    idt.breakpoint.set_handler_fn(breakpoint_handler);
-}
-
 extern "x86-interrupt" fn breakpoint_handler(
     stack_frame: InterruptStackFrame)
 {
