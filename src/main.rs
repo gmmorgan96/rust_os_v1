29e0a6ccc63c72a61559d92e658ea234d5c9a1d0
gmmorgan1996 <griffin.mrogan@gmail.com>
2024-02-06 11:02:06 -0500

    chore: test

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,50 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(blog_os::test_runner)]
-#![reexport_test_harness_main = "test_main"]
+#![feature(abi_x86_interrupt)]
 
-use blog_os::println;
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+mod interrupts;
+mod serial;
+
 #[no_mangle]
-pub extern fn rust_main {
-    println!("Hello World{}", "!");
+pub extern fn rust_main() {
+    // ATTENTION: we have a very small stack and no guard page
 
-    #[cfg(test)]
-    test_main();
+    // let hello = b"Hello World!";
+    // let color_byte = 0x1f; // white foreground, blue background
 
-    loop {}
+    // let mut hello_colored = [color_byte; 24];
+    // for (i, char_byte) in hello.into_iter().enumerate() {
+    //     hello_colored[i*2] = *char_byte;
+    // }
+
+    // // write `Hello World!` to the center of the VGA text buffer
+    // let buffer_ptr = (0xb8000 + 1988) as *mut _;
+    // unsafe { *buffer_ptr = hello_colored };
+    // vga_buffer::print_something();
+    // use core::fmt::Write;
+    // vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    // write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
+    println!("Hello This is a Test{}", "!");
+    // init(); // new
+
+    // invoke a breakpoint exception
+    // x86_64::instructions::interrupts::int3(); // new
+    println!("It did not crash!");
+    loop{}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
+// #[lang = "eh_personality"] extern fn eh_personality() {}
+// #[lang = "panic_fmt"] #[no_mangle] pub extern fn panic_fmt() -> ! {loop{}}
+
+pub fn init() {
+    interrupts::init_idt();
+}
+
 #[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
+fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
-}
-
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    blog_os::test_panic_handler(info)
-}
-
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
 }
